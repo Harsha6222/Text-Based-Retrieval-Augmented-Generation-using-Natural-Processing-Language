3234649d04157d74e2a388d305a1f5e26f77e12a
G v s Harsha Vardhan Reddy <155625047+Harsha6222@users.noreply.github.com>
2024-09-25 18:54:32 +0530

    Add files via upload

---
+++ __0__.py
@@ -0,0 +1,62 @@
+import pypdf
+import requests
+import parsel
+from langchain_google_genai import ChatGoogleGenerativeAI as lcgai
+
+
+def func__ip():
+    print(" = "*20)
+    print("{:-^60}".format(" Input "))
+    print("Select the type of input ::")
+    print("\t1. Text File\n\t2. PDF File\n\t3. HTML File [Web : URL]")
+    ip = int(input("[ip] Enter the type of input file [numeric] :: ") or '2')
+    print(" - "*20)
+    if(0):
+        ...
+    elif(ip == 1):
+        ip = (input("[ip] Enter the path of the text file :: ") or '0/CSC__02/txt_wiki__DataScience.txt')
+        with open(ip, 'r') as f:
+            _p_ = f.read()
+    elif(ip == 2):
+        ip = (input("[ip] Enter the path of the pdf file :: ") or '0/CSC__02/pdf_wiki__DataScience.pdf')
+        f = pypdf.PdfReader(ip).pages[:]
+        _p_ = "".join([itr.extract_text() for itr in f])
+    elif(ip == 3):
+        ip = (input("[ip] Enter the url of the web page :: ") or 'https://en.wikipedia.org/wiki/Data_science')
+        f = requests.get(ip).text
+        _p_ = "".join(parsel.Selector(f, type="html").xpath("//body//text()").getall())
+    else:
+        ...
+    print(" = "*20)
+    print("{:-^60}".format(" Process "))
+    print("[logs] :: `Processing ...`")
+    func__op(io=_p_)
+
+
+def func__p(io=' ',prompt=' '):
+    _ip_ = io
+    __p__ = lcgai(google_api_key='AIzaSyC076g-GBMvHsRqzPa_QtxotOoOkxEdZ6s',model="gemini-pro",max_output_tokens=8888)
+    _op_ = __p__.invoke(f'{prompt} : {_ip_}')
+    return _op_
+
+
+def func__op(io=' '):
+    print(" = "*20)
+    print("{:-^60}".format(" Output "))
+    ip = ''
+    while(ip!='exit'):
+        ip = input("[ip] Enter the prompt [`exit` to terminate] :: ") or 'exit'
+        print(" - "*20)
+        if(ip=='exit'):
+            print("[logs] :: `Terminating ...`")
+            continue
+        _p_ = func__p(io=io,prompt=ip)
+        for op in _p_:
+            print(op[1])
+            break
+        print(" *-* "*20)
+    print(" = "*20)
+
+
+if(__name__ == '__main__'):
+    func__ip()
---
+++ __1__.py
@@ -0,0 +1,59 @@
+import os
+import pypdf
+import requests
+import parsel
+from langchain_google_genai import ChatGoogleGenerativeAI as lcgai
+from shiny import ui, reactive, render, App, run_app
+from multiprocessing import Process, freeze_support
+
+cwd = os.getcwd().replace("\\","/")
+
+def _0_(_ip_):
+    if(0):
+        ...
+    elif(_ip_.sip_file_url() == "Text File" and _ip_.ip_file_url().endswith("txt")):
+        with open(_ip_.ip_file_url(), 'r') as f:
+            ip = f.read()
+    elif(_ip_.sip_file_url() == "PDF File" and _ip_.ip_file_url().endswith("pdf")):
+        f = pypdf.PdfReader(_ip_.ip_file_url()).pages[:]
+        ip = "".join([itr.extract_text() for itr in f])
+    elif(_ip_.sip_file_url() == "HTML File [Web : URL]" and (_ip_.ip_file_url().startswith("http") or _ip_.ip_file_url().endswith("html"))):
+        f = requests.get(_ip_.ip_file_url()).text
+        ip = "".join(parsel.Selector(f, type="html").xpath("//body//text()").getall())
+    elif(1):
+        ip = None
+    return(ip)
+
+
+def _8_(_ip_, _op_, _p_):
+    @_op_
+    @render.text
+    @reactive.event(_ip_.ip_ab_submit)
+    def op_rag():
+        ip = _0_(_ip_)
+        if(ip != None):
+            p = lcgai(google_api_key='AIzaSyC076g-GBMvHsRqzPa_QtxotOoOkxEdZ6s',model="gemini-pro",max_output_tokens=8888)
+            op = p.invoke(f'{_ip_.ip_prompt} : {ip}').content.replace("**","")
+        if(ip == None):
+            op = "ERROR :: Invalid Input"
+        return(op)
+    ...
+
+def _1_():
+    return \
+        ui.page_fluid(
+            ui.include_css(f'{cwd}/8/CSC__03/__1__.css'),
+            ui.input_select("sip_file_url",label="Select the type of input ::",choices=["Text File","PDF File","HTML File [Web : URL]"],selected="HTML File [Web : URL]"),
+            ui.input_text("ip_file_url",label="Enter the file/URL ::",value="https://en.wikipedia.org/wiki/Data_science"),
+            ui.input_text("ip_prompt",label="Enter the prompt ::",value="summarize the data"),
+            ui.input_action_button("ip_ab_submit","RAG"),
+            ui.HTML("<br>"),
+            ui.output_text_verbatim("op_rag")
+    )
+    
+_081_ = App(_1_(),_8_)
+
+
+if __name__ == '__main__':
+    freeze_support() # (optional)
+    Process(target=run_app('__1__:_081_',reload=True)).start() # (required) ||  < (reload=True) :: for debugging >
